9543383a3545e7c3d9166924da32d3649f1a9162
Ravera Thibault <ravera.thibault+github@gmail.com>
2017-12-13 21:23:40 -0800

    Add codecov

--- runtests.py
+++ runtests.py
@@ -11,6 +11,7 @@ import sys
 
 PYTEST_ARGS = {
     'default': ['tests'],
+    'fast': ['tests', '-q']
 }
 
 
@@ -23,5 +24,32 @@ def exit_on_failure(ret, message=None):
 
 
 if __name__ == '__main__':
-    pytest_args = PYTEST_ARGS['default'] + ['-s']
+    try:
+        sys.argv.remove('--fast')
+    except ValueError:
+        style = 'default'
+    else:
+        style = 'fast'
+
+    if len(sys.argv) > 1:
+        pytest_args = sys.argv[1:]
+        first_arg = pytest_args[0]
+
+        try:
+            pytest_args.remove('--coverage')
+        except ValueError:
+            pass
+        else:
+            pytest_args = ['--cov-report',
+                           'xml',
+                           '--cov',
+                           'rt_ip'] + pytest_args
+
+        if first_arg.startswith('-'):
+            # `runtests.py [flags]`
+            pytest_args = ['tests'] + pytest_args
+
+    else:
+        pytest_args = PYTEST_ARGS[style]
+
     exit_on_failure(pytest.main(pytest_args))
