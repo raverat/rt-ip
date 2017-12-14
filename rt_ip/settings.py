f47480bf9320bc8dcea919791fbaa5616f07be7a
Ravera Thibault <ravera.thibault+github@gmail.com>
2017-12-13 21:38:56 -0800

    Remove useless settings file

--- rt_ip/settings.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-
-RT_IP_LOCALHOST = getattr(settings, 'RT_IP_LOCALHOST', '127.0.0.1')
-RT_IP_SUBSTITUTES = getattr(settings, 'RT_IP_SUBSTITUTES', {})
-RT_IP_SUBSTITUTES['127.0.0.1'] = RT_IP_LOCALHOST
-RT_IP_SUBSTITUTES['localhost'] = RT_IP_LOCALHOST
